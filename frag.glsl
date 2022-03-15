34bbfcbe608e6a26d958ffdaa47c779bb927bf76
Ptolom <47869153+Homo-carbonis@users.noreply.github.com>
2022-03-15 09:08:18 +0000

    licence

--- frag.glsl
+++ frag.glsl
@@ -1,3 +1,17 @@
+// Copyright Ptolom 2022
+// This file is part of Trophic.
+//
+// Trophic is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with this
+// program. If not, see <https://www.gnu.org/licenses/>. 
+
 #version 460 core
 
 layout(location=0) out vec4 colour;
@@ -8,6 +22,4 @@ in vec3 textureCoordinate;
 
 void main() {
   colour = texture(tex, textureCoordinate);
-  //colour = vec4(textureCoordinate,1.0);
-  //colour = vec4(1.0,1.0,0.0,1.0);
 }
