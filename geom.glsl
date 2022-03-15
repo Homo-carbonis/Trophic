6012de7a01277f51bc2a3c05d1e728f7ce156ad0
Ptolom <47869153+Homo-carbonis@users.noreply.github.com>
2022-03-15 09:08:56 +0000

    licence

--- geom.glsl
+++ geom.glsl
@@ -1,3 +1,17 @@
+// Copyright Ptolom 2022
+// This file is part of Trophic.
+//
+// Trophic is free software: you can redistribute it and/or modify it under the
+// terms of the GNU General Public License as published by the Free Software
+// Foundation, either version 3 of the License, or (at your option) any later version.
+// 
+// This program is distributed in the hope that it will be useful, but WITHOUT ANY
+// WARRANTY; without even the implied warranty of MERCHANTABILITY or
+// FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+// 
+// You should have received a copy of the GNU General Public License along with this
+// program. If not, see <https://www.gnu.org/licenses/>.
+
 #version 460 core
 
 layout(points) in;
@@ -15,7 +29,6 @@ void main()
     gl_Position.xyw = transform[0] * clipSquare[i];
     gl_Position.z=0.0;
     textureCoordinate.xy=textureSquare[i];
-    //textureCoordinate.z=frame[0];
     textureCoordinate.z=float(frame[0]);
     EmitVertex();
   }
